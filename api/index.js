6c37b4a53485bc5516977449f6fddc90863f7764
MeHO <pyaekyaw145@gmail.com>
2026-05-18 21:41:25 +0700

    Gemini Model Change

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
     const genAI = new GoogleGenerativeAI(API_KEY);
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-2.5-flash-lite",
+      model: "gemini-1.5-flash",
     });
 
     const { type } = req.query;
